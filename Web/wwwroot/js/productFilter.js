d28163ca1351d04b1fc516190c29dad2d79deef8
voron <voron@LAPTOP-GT3KOQPJ>
2023-08-07 21:59:20 +0300

    Filter by Price

---
+++ Web/wwwroot/js/productFilter.js
@@ -0,0 +1,15 @@
+﻿var SITE_PATH = "https://localhost:7173/";
+
+$("#isRecomendedFilter").change(function () {
+    let categoryId = $("#categoryIdInput").val();
+
+    $.ajax({
+        url: SITE_PATH + 'customer/home/filter',
+        type: 'GET',
+        data: { categoryId: categoryId, isRecomended: this.checked },
+        dataType: 'html',
+        success: function (data) {
+            $('body').html(data);
+        },
+    });
+});
